3768d8653578078f90b1fad3df8f8be5fa8386b9
Erik Kurjak <erik.kurjak@ceskapojistovna.cz>
2018-12-10 22:30:46 +0100

    Little nit of refactoring of files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Music from './Music/Music';
 import * as serviceWorker from './serviceWorker';
-
-// Importing CSS file
 import './App.css';
 
 const ROOT_ELEMENT = document.getElementById('root');
@@ -14,7 +11,4 @@ ReactDOM.render(
     ROOT_ELEMENT
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
