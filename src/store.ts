24a7d948f9932928c0a7eaa18bc8b44747fb95e0
Erik Kurjak <erik.kurjak@ceskapojistovna.cz>
2018-12-12 22:30:18 +0100

    Playing with the store - redux dev-tools and logger to console

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,33 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk  from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from './reducer';
 
+const INITIAL_DATA = {
+    status: 'initial',
+    artists: [],
+    temp: 'john',
+    error: null,
+};
+
+// TODO
+const logger = ({ getState }: any) => {
+    return (next: any) => (action: any) => {
+      console.log('will dispatch', action);
+      const returnValue = next(action);
+      console.log('state after dispatch', getState());
+      return returnValue;
+    }
+}
+
 export default createStore(
     reducer,
-    {
-        status: 'initial',
-        artists: [],
-        temp: 'john',
-        error: null,
-    },
-    applyMiddleware(thunk)
+    INITIAL_DATA,
+    composeWithDevTools(
+        applyMiddleware(
+            thunk,
+            logger
+        )
+    )
 );
